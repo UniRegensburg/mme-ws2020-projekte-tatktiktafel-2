ab8b7b4c55d6a2fd117279bf428b30d41e76393e
Alexander Gebhard <alexander.gebhard@posteo.de>
2021-03-07 17:17:40 +0100

    erster ansatz mit verschiedenen raeumen, noch nicht funktionierend

--- index.js
+++ index.js
@@ -3,61 +3,195 @@ var http = require('http');
 var ws = require('ws');
 var uuid = require('uuid');
 const path = require('path');
+var jwt = require('jsonwebtoken');
+
+var bodyParser = require('body-parser')
 
 
-//s
+var dotenv = require('dotenv');
+const { env } = require('process');
+// https://www.twilio.com/blog/working-with-environment-variables-in-node-js-html
+dotenv.config();
 
 const app = express();
-app.use(express.static(`${__dirname}/resources`));
-app.locals.connections = [];
+app.use('/resources', express.static(`${__dirname}/resources`));
+app.use('/favicon.ico', express.static('${__dirname}/resources/img/gun_PEa_icon.ico'));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const server = http.createServer(app);
-const wss = new ws.Server({ server });
-
-function broadcastConnections() {
-    let ids = app.locals.connections.map(c => c._connId);
-    app.locals.connections.forEach(c => {
-        c.send(JSON.stringify({ type: 'ids', ids }));
-    });
-}
-
-console.log("hier")
-
-wss.on('connection', (ws) => {
-    app.locals.connections.push(ws);
-    ws._connId = `conn-${uuid.v4()}`;
-
-    // send the local id for the connection
-    ws.send(JSON.stringify({ type: 'connection', id: ws._connId }));
-
-    // send the list of connection ids
-    broadcastConnections();
-
-    ws.on('close', () => {
-        let index = app.locals.connections.indexOf(ws);
-        app.locals.connections.splice(index, 1);
-
-        // send the list of connection ids
-        broadcastConnections();
-    });
-
-    ws.on('message', (message) => {
-        for (let i = 0; i < app.locals.connections.length; i++) {
-            if (app.locals.connections[i] !== ws) {
-                app.locals.connections[i].send(message);
-            }
-        }
-    });
-
-});
-
-
-app.get('/', (req, res) => {
-    //res.sendFile(path.join(__dirname, 'index.html'));
-    console.log("afterglow")
-    res.sendFile(path.join(__dirname, 'resources/html/index.html'));
-});
-
 server.listen(process.env.PORT || 8081, () => {
     console.log(`Started server on port ${server.address().port}`);
 });
+
+// starting index
+app.locals.index = 100000000000;
+
+var clients = {};
+var channels = {};
+
+
+app.get('/', (req, res) => {
+    console.log("app.get('/'")
+    app.locals.index++;
+    let id = app.locals.index.toString(36);
+    res.redirect(`/${id}`);
+});
+
+
+app.get('/connect', (req,res) => {
+    console.log("app.get('/connect'")
+    if (req.headers.accept !== 'text/event-stream') {
+        return res.sendStatus(404);
+    }
+    // write the event stream headers
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.flushHeaders();
+    //console.log(req)
+    // setup a client
+
+    var decode = jwt.verify(req.query.token, process.env.TOKEN_SECRET);
+    //console.log(decode)
+
+    let client = {
+        id: decode.id,
+        user: decode.username,
+        emit: (event, data) => {
+            res.write(`id: ${uuid.v4()}`);
+            res.write(`event: ${event}`);
+            res.write(`data: ${JSON.stringify(data)}`);
+        }
+    };
+
+    clients[client.id] = client;
+
+
+    req.on('close', () => {
+        disconnected(client);
+    });
+
+    //res.write("connected")
+   
+});
+
+app.post('/access', (req, res) => {
+    console.log("app.post('/access'")
+    //console.log(req.body.username)
+    if (!req.body.username) {
+        return res.sendStatus(403);
+    }
+    const user = {
+        id: uuid.v4(),
+        username: req.body.username
+    };
+
+    const token = jwt.sign(user,process.env.TOKEN_SECRET,{  expiresIn: '3600s' });
+    //console.log(token)
+    return res.json(token);
+});
+
+
+app.get('/:roomId', (req, res) => {
+    console.log("app.get('/:roomId'")
+    res.sendFile(path.join(__dirname, 'resources/html/index.html'));
+});
+
+
+
+// store the connections from clients here
+
+function disconnected(client) {
+    let index = app.locals.clients.indexOf(client);
+    if (index > -1) {
+        app.locals.clients.splice(index, 1);
+    }
+}
+
+
+
+app.post('/:roomId/join', auth, (req, res) => {
+    console.log("app.post('/:roomId/join'")
+    let roomId = req.params.roomId;
+    console.log(req.params)
+    if (channels[roomId] && channels[roomId][req.user.id]) {
+        return res.sendStatus(200);
+    }
+    if (!channels[roomId]) {
+        channels[roomId] = {};
+    }
+
+    
+    console.log("channels:" + channels)
+    for (peerId in channels) {
+        console.log("peerID: " + peerID)
+        console.log("ReqUserID: " + req.user.id)
+
+        console.log("links: " + clients[peerId])
+        console.log("rechts: " + clients[req.user.id])
+
+
+        console.log(clients)
+        console.log(channels)
+
+        if (clients[peerID] && clients[req.user.id]) {
+            console.log("connection")
+            clients[peerId].emit('add-peer', { peer: req.user, roomId, offer: false });
+            clients[req.user.id].emit('add-peer', { peer: clients[peerID].user, roomId, offer: true });
+        }
+    }
+
+    channels[roomId][req.user.id] = true;
+    return res.sendStatus(200);
+});
+
+app.post('/relay/:peerId/:event', auth, (req, res) => {
+    let peerId = req.params.peerId;
+    if (clients[peerId]) {
+        clients[peerId].emit(req.params.event, { peer: req.user, data: req.body });
+    }
+    return res.sendStatus(200);
+});
+
+function disconnected(client) {
+    delete clients[client.id];
+    for (let roomId in channels) {
+        let channel = channels[roomId];
+        if (channel[client.id]) {
+            for (let peerId in channel) {
+                channel[peerId].emit('remove-peer', { peer: client.user, roomId });
+            }
+            delete channel[client.id];
+        }
+        if (Object.keys(channel).length === 0) {
+            delete channels[roomId];
+        }
+    }
+}
+
+function auth(req, res, next) {
+    console.log("auth function")
+    let token;
+    if (req.headers.authorization) {
+        token = req.headers.authorization.split(' ')[1];
+    } else if (req.query.token) {
+        token = req.query.token;
+    }
+    if (typeof token !== 'string') {
+        return res.sendStatus(401);
+    }
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        req.user = user;
+        next();
+    });
+}
+
+
+
+
