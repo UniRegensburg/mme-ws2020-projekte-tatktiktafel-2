79746da2af64a6579653bc99c83a42e0119f096c
Alexander Gebhard <alexander.gebhard@posteo.de>
2021-03-31 16:39:12 +0200

    final_01

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var http = require('http');
+var ws = require('ws');
 var uuid = require('uuid');
 const path = require('path');
 var jwt = require('jsonwebtoken');
 var bodyParser = require('body-parser');
-
-
 var dotenv = require('dotenv');
+
 const { env } = require('process');
 // https://www.twilio.com/blog/working-with-environment-variables-in-node-js-html
 dotenv.config();
@@ -14,20 +14,61 @@ dotenv.config();
 const app = express();
 app.use('/resources', express.static(`${__dirname}/resources`));
 app.use('/favicon.ico', express.static('${__dirname}/resources/img/gun_PEa_icon.ico'));
-
+app.locals.connections = [];
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 const server = http.createServer(app);
-server.listen(process.env.PORT || 8081, () => {
-    console.log(`Started server on port ${server.address().port}`);
-});
+const wss = new ws.Server({ server });
 
-// starting index
 app.locals.index = 100000000000;
 
 var clients = {};
 var channels = {};
+
+function broadcastConnections() {
+    let ids = app.locals.connections.map(c => c._connId);
+    app.locals.connections.forEach(c => {
+        c.send(JSON.stringify({ type: 'ids', ids }));
+    });
+}
+
+console.log("hier")
+
+wss.on('connection', (ws) => {
+    app.locals.connections.push(ws);
+    ws._connId = `conn-${uuid.v4()}`;
+
+    // send the local id for the connection
+    ws.send(JSON.stringify({ type: 'connection', id: ws._connId }));
+
+    // send the list of connection ids
+    broadcastConnections();
+
+    ws.on('close', () => {
+        let index = app.locals.connections.indexOf(ws);
+        app.locals.connections.splice(index, 1);
+
+        // send the list of connection ids
+        broadcastConnections();
+    });
+
+    ws.on('message', (message) => {
+        for (let i = 0; i < app.locals.connections.length; i++) {
+            if (app.locals.connections[i] !== ws) {
+                app.locals.connections[i].send(message);
+            }
+        }
+    });
+
+});
+
+
+/*app.get('/', (req, res) => {
+    //res.sendFile(path.join(__dirname, 'index.html'));
+    console.log("afterglow")
+    res.sendFile(path.join(__dirname, 'resources/html/index.html'));
+});*/
 
 
 app.get('/', (req, res) => {
@@ -37,174 +78,12 @@ app.get('/', (req, res) => {
     res.redirect(`/${id}`);
 });
 
-
-app.get('/connect', (req,res) => {
-    console.log("app.get('/connect'")
-    if (req.headers.accept !== 'text/event-stream') {
-        return res.sendStatus(404);
-    }
-    // write the event stream headers
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.write("event: " + "open\n\n")
-
-    // setup a client
-
-    var decode = jwt.verify(req.query.token, process.env.TOKEN_SECRET);
-
-    let client = {
-        id: decode.id,
-        user: decode.username,
-        emit: (event, data) => {
-            console.log("im emmit")
-            res.write(`event: ${event}\ndata: ${JSON.stringify(data)}\nid: ${uuid.v4()}\n\n`)
-            //res.write("id: " + uuid.v4() + "\n\n");
-            //res.write("event: " + event + "\n\n");
-            //res.write("data: " + JSON.stringify(data) + "\n\n");
-        }
-    };
-
-    clients[client.id] = client;
-    req.on('close', () => {
-        disconnected(client);
-    });
-   
-});
-
-app.post('/access', (req, res) => {
-    console.log("app.post('/access'")
-    if (!req.body.username) {
-        return res.sendStatus(403);
-    }
-    const user = {
-        id: uuid.v4(),
-        username: req.body.username
-    };
-    const token = jwt.sign(user,process.env.TOKEN_SECRET,{  expiresIn: '3600s' });
-    console.log(token)
-    return res.json(token);
-});
-
-
 app.get('/:roomId', (req, res) => {
     console.log("app.get('/:roomId'")
     res.sendFile(path.join(__dirname, 'resources/html/index.html'));
 });
 
 
-app.post('/:roomId/join', auth, (req, res) => {
-    console.log("app.post('/:roomId/join'")
-    let roomId = req.params.roomId;
-    clients[req.user.id]["roomId"] = roomId
-    console.log("Channels: ")
-    console.log(channels)
-    for (let peerId in channels) {
-        
-        console.log("links: ")
-        console.log((channels[peerId]))
-        console.log("Rechts: ")
-        console.log(clients[req.user.id])
-        if ((channels[peerId]) && clients[req.user.id]) {
-            console.log("connection")
-
-            console.log("clients: ")
-            console.log(clients)
-
-            //user der joined
-            console.log("User der Joined")
-            clients[req.user.id].emit('add-peer', { roomId, offer: true, peer: req.user, control: 1 });
-
-            console.log(channels[roomId])
-
-            Object.keys(channels[roomId]).forEach(function(key,index) {
-                
-                clients[key].emit('add-peer', {  roomId, offer: false, peer: req.user, control: 2 });
-                // key: the name of the object key
-                // index: the ordinal position of the key within the object 
-            });
-        }
-    }
-    let roomUser = {}
-    roomUser[req.user.id] = {}
-    roomUser[req.user.id] = true
-    channels[roomId] = {}
-    channels[roomId] = roomUser
-    return res.sendStatus(200);
+server.listen(process.env.PORT || 8081, () => {
+    console.log(`Started server on port ${server.address().port}`);
 });
-
-app.post('/relay/:peerId/:event', auth, (req, res) => {
-    console.log('/relay/:peerId/:event')
-    console.log("event: ")
-    console.log(req.params.event)
-    console.log("req: ")
-    //console.log(req.body)
-    let peerId = req.params.peerId;
-    console.log(req.body.type)
-    console.log(clients)
-    console.log("peerID: ")
-    console.log(peerId)
-    Object.keys(clients).forEach(function(key) {
-        if (clients[key].roomId == peerId) {
-            console.log("true")
-            clients[key].emit(req.params.event, { peer: req.user, data: req.body });    
-        }
-    }); 
-    /*
-    clients.forEach(function(item){
-        console.log("im foreach block")
-        if (item.roomId == peerId) {
-            item.emit(req.params.event, { peer: req.user, data: req.body });    
-        }
-    });
-
-    
-    if (clients[peerId]) {
-
-        clients[peerId].emit(req.params.event, { peer: req.user, data: req.body });
-    }*/
-    return res.sendStatus(200);
-});
-
-function disconnected(client) {
-    delete clients[client.id];
-    for (let roomId in channels) {
-        let channel = channels[roomId];
-        if (channel[client.id]) {
-            for (let peerId in channel) {
-                console.log("RemoveChannelPeriod:")
-                console.log(channel[peerId])
-                channel[peerId].emit('remove-peer', { peer: client.user, roomId });
-            }
-            delete channel[client.id];
-        }
-        if (Object.keys(channel).length === 0) {
-            delete channels[roomId];
-        }
-    }
-}
-
-function auth(req, res, next) {
-    console.log("auth function")
-    let token;
-    if (req.headers.authorization) {
-        token = req.headers.authorization.split(' ')[1];
-    } else if (req.query.token) {
-        token = req.query.token;
-    }
-    if (typeof token !== 'string') {
-        return res.sendStatus(401);
-    }
-
-    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-        if (err) {
-            return res.sendStatus(403);
-        }
-        req.user = user;
-        next();
-    });
-}
-
-
-
-
--- resources/js/load.js
+++ resources/js/load.js
@@ -1,267 +1,79 @@
-var context = {
-    username: 'user' + parseInt(Math.random() * 100000),
-    roomId: window.location.pathname.substr(1),
-    token: null,
-    eventSource: null,
-    peers: [],
-    channels: [],
-    test: {}
+const wsConnection = new WebSocket(('ws:127.0.0.1:8081/' + window.location.pathname.substr(1)), 'json');
+//wenn ich seite aufrufe, dann wird versucht verbindung herzsutellen
+// der untere kack wird dann ausgegeben
+wsConnection.onopen = (e) => {
+    console.log(`wsConnection open to 127.0.0.1:8081`, e);
 };
-
-async function getToken() {
-    let res = await fetch('/access', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: context.username
-        })
-    });
-    
-    
-    let data = await res.json();
-    context.token = data;
-}
-
-async function join() {
-    console.log("hier2")
-    return fetch(`/${context.roomId}/join`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${context.token}`
-        }
-    });
-}
-
-async function connect() {
-    await getToken();
-    context.eventSource = new EventSource(`/connect?token=${context.token}`);
-    context.eventSource.addEventListener('add-peer', (e) => {
-        console.log("add-peer")
-        console.log(e)
-        addPeer(e)}, false);
-    context.eventSource.addEventListener('remove-peer', (e) => {
-        console.log("remove-peer")
-        console.log(e)
-        removePeer(e)}, false);
-    context.eventSource.addEventListener('session-description', (e) => {
-        console.log("session-description")
-        console.log(e)
-        sessionDescription(e)}, false);
-    context.eventSource.addEventListener('ice-candidate', (e) => {
-        console.log("ice-candidate")
-        console.log(e)
-        iceCandidate(e)}, false);
-    context.eventSource.addEventListener('open', () => {
-        console.log("open")
-        join();
-    });
-    context.eventSource.onmessage = console.log
-    context.eventSource.addEventListener('error', e => {
-        if (e.readyState === EventSource.CLOSED) {
-         console.log('Connection was closed! ', e);
-        } else {
-         console.log('An unknown error occurred: ', e);
-        }
-       }, false);
-}
-
-const rtcConfig = {
-    iceServers: [{
-        urls: [
-            'stun:stun.l.google.com:19302',
-            'stun:stun1.l.google.com:19302'
-        ]
-    }]
+wsConnection.onerror = (e) => {
+    console.error(`wsConnection error `, e);
+};
+wsConnection.onmessage = (e) => {
+    console.log(JSON.parse(e.data));
 };
 
 
 
-function addPeer(data) {
-    console.log("addPeer")
-    let message = JSON.parse(data.data);
-    console.log("message: ")
-    console.log(message)
-    console.log("own context: ")
-    console.log(context)
 
-    // setup peer connection
-    let peer = new RTCPeerConnection(rtcConfig);
-    //context.peers = []
-    context.peers[message.peer.id] = peer;
+//zweiter Teil
 
-    // handle ice candidate
-    peer.onicecandidate = function (event) {
-        if (event.candidate) {
-            relay(message.peer.id, 'ice-candidate', event.candidate);
+var localId, peerIds;
+var peerConnections = {};
+var initiator = false;
+
+wsConnection.onmessage = (e) => {
+    let data = JSON.parse(e.data);
+    switch (data.type) {
+        case 'connection':
+            localId = data.id;
+            break;
+        case 'ids':
+            peerIds = data.ids;
+            connect();
+            break;
+        case 'signal':
+            signal(data.id, data.data);
+            break;
+    }
+};
+
+function onPeerData(id, data) {
+    console.log(`data from ${id}`, data);
+}
+
+function connect() {
+    // cleanup peer connections not in peer ids
+    Object.keys(peerConnections).forEach(id => {
+        if (!peerIds.includes(id)) {
+            peerConnections[id].destroy();
+            delete peerConnections[id];
         }
-    };
-
-    // generate offer if required (on join, this peer will create an offer
-    // to every other peer in the network, thus forming a mesh)
-    if (message.offer) {
-        console.log('Created local peer connection object pc1');
-        //peer.addEventListener('icecandidate', e => onIceCandidate(peer, e));
-        console.log("offer true")
-        peer.addEventListener('iceconnectionstatechange', e => onIceStateChange(peer, e));
-        // create the data channel, map peer updates
-        let channel = peer.createDataChannel('updates');
-        channel.onmessage = function (event) {
-            onPeerData(message.peer.id, event.data);
-        };
-        //context.
-        console.log('pc1 createOffer start');
-        context.channels[message.peer.id] = channel;
-        createOffer(context.roomId, peer);
-        //createOffer(message.peer.id, peer);
-    } else {
-        console.log("offer false")
-        console.log('Created remote peer connection object pc2');
-        //peer.addEventListener('icecandidate', e => onIceCandidate(peer, e));
-        peer.addEventListener('iceconnectionstatechange', e => onIceStateChange(peer, e));
-        
-        peer.ondatachannel = function (event) {
-            context.channels[message.peer.id] = event.channel;
-            event.channel.onmessage = function (evt) {
-                onPeerData(message.peer.id, evt.data);
-            };
-        };
+    });
+    if (peerIds.length === 1) {
+        initiator = true;
     }
-}
+    peerIds.forEach(id => {
+        if (id === localId || peerConnections[id]) {
+            return;
+        }
 
-function broadcast(data) {
-    console.log("es wird data gesendet")
-    for (let peerId in context.channels) {
-        context.channels[peerId].send(data);
-    }
-}
-
-async function relay(peerId, event, data) {
-    //data ist hier offer
-    console.log("relay function")
-    console.log("data: ")
-    console.log(data)
-    context.test = data;
-    console.log(JSON.stringify(data))
-    await fetch(`/relay/${peerId}/${event}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${context.token}`
-        },
-        body: JSON.stringify(data)
+        let peer = new SimplePeer({
+            initiator: initiator
+        });
+        peer.on('error', console.error);
+        peer.on('signal', data => {
+            wsConnection.send(JSON.stringify({
+                type: 'signal',
+                id: localId,
+                data
+            }));
+        });
+        peer.on('data', (data) => onPeerData(id, data));
+        peerConnections[id] = peer;
     });
 }
 
-async function createOffer(peerId, peer) {
-    console.log("create-offer function")
-    //let offer = await peer.createOffer();
-    let offer = await peer.createOffer();
-    //await onCreateOfferSuccess(offer);
-    //hier simma
-    await peer.setLocalDescription(offer);
-    onSetLocalSuccess(peer);
-
-    await relay(peerId, 'session-description', offer);
-}
-
-function onSetLocalSuccess(pc) {
-    console.log(`${(pc)} setLocalDescription complete`);
-  }
-
-function onSetSessionDescriptionError(error) {
-console.log(`Failed to set session description: ${error.toString()}`);
-}
-function onAddIceCandidateSuccess(pc) {
-    console.log(`${(pc)} addIceCandidate success`);
-  }
-  
-function onAddIceCandidateError(pc, error) {
-console.log(`${pc} failed to add ICE Candidate: ${error.toString()}`);
-}
-
-async function onCreateOfferSuccess(desc) {
-
-    console.log('pc2 setRemoteDescription start');
-    try {
-      await pc2.setRemoteDescription(desc);
-      onSetRemoteSuccess(pc2);
-    } catch (e) {
-      onSetSessionDescriptionError();
+function signal(id, data) {
+    if (peerConnections[id]) {
+        peerConnections[id].signal(data);
     }
-  
-    console.log('pc2 createAnswer start');
-    // Since the 'remote' side has no media stream we need
-    // to pass in the right constraints in order for it to
-    // accept the incoming offer of audio and video.
-    try {
-      const answer = await pc2.createAnswer();
-      await onCreateAnswerSuccess(answer);
-    } catch (e) {
-      onCreateSessionDescriptionError(e);
-    }
-  }
-
-
-async function sessionDescription(data) {
-    console.log("sessionDescriptionOffer")
-    let message = JSON.parse(data.data);
-    let peer = context.peers[message.peer.id];
-
-    let remoteDescription = new RTCSessionDescription(message.data);
-    await peer.setRemoteDescription(remoteDescription);
-    if (remoteDescription.type === 'offer') {
-        console.log("sD offer Block")
-        console.log("msg.peer.id")
-        console.log(message.peer.id)
-        let answer = await peer.createAnswer();
-        await peer.setLocalDescription(answer);
-        await relay(message.peer.id, 'session-description', answer);
-    }
-    else if (remoteDescription.type === 'answer'){
-        console.log("sD answer Block")
-    }
-    
 }
-
-function iceCandidate(data) {
-    console.log("iceCandidate-function")
-    //let message = data.peer;
-    let message = JSON.parse(data.data);
-    console.log(message)
-    console.log(message.peer.id)
-    let peer = context.peers[message.peer.id];
-    console.log(peer)
-    peer.addIceCandidate(new RTCIceCandidate(message.data));
-}
-
-function removePeer(data) {
-    let message = JSON.parse(data.data);
-    if (context.peers[message.peer.id]) {
-        context.peers[message.peer.id].close();
-    }
-
-    delete context.peers[message.peer.id];
-}
-
-async function onIceCandidate(pc, event) {
-    try {
-      await (pc.addIceCandidate(event.candidate));
-      onAddIceCandidateSuccess(pc);
-    } catch (e) {
-      onAddIceCandidateError(pc, e);
-    }
-    console.log(`${(pc)} ICE candidate:\n${event.candidate ? event.candidate.candidate : '(null)'}`);
-  }
-
-function onIceStateChange(pc, event) {
-if (pc) {
-    console.log(`${(pc)} ICE state: ${pc.iceConnectionState}`);
-    console.log('ICE state change event: ', event);
-}
-}
-
-
-connect();
